1b08ee7515c41d81c8687252a331f0e55cdab583
sommaanny <a000626@naver.com>
2026-05-02 20:08:12 +0900

    dev exception and errorCode

--- src/main/java/com/liveclass/be_a/global/exception/BusinessException.java
+++ src/main/java/com/liveclass/be_a/global/exception/BusinessException.java
@@ -1,2 +1,13 @@
-package com.liveclass.be_a.domain.course.repository;public class BusinessException {
+package com.liveclass.be_a.global.exception;
+
+/**
+ * 커스텀 런타임 예외
+ */
+public class BusinessException extends RuntimeException{
+    private final ErrorCode errorCode;
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage()); //ErrorCode에 정의된 메시지로 예외 생성
+        this.errorCode = errorCode;
+    }
 }
--- src/main/java/com/liveclass/be_a/global/exception/ErrorCode.java
+++ src/main/java/com/liveclass/be_a/global/exception/ErrorCode.java
@@ -1,2 +1,26 @@
-package com.liveclass.be_a.global.exception;public class ErrorCode {
+package com.liveclass.be_a.global.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+    COURSE_NOT_DRAFT(HttpStatus.CONFLICT, "COURSE_NOT_DRAFT", "Only draft classes can be changed."),
+    COURSE_ALREADY_CLOSED(HttpStatus.CONFLICT, "CLASS_ALREADY_CLOSED", "Class is already closed."),
+    ENROLLMENT_NOT_PENDING(HttpStatus.CONFLICT, "ENROLLMENT_NOT_PENDING", "Only pending enrollments can be confirmed."),
+    ENROLLMENT_ALREADY_CANCELLED(HttpStatus.CONFLICT, "ENROLLMENT_ALREADY_CANCELLED", "Enrollment is already cancelled."),
+    ENROLLMENT_NOT_CANCELLED(HttpStatus.CONFLICT, "ENROLLMENT_NOT_CANCELLED", "ReEnrollment is only possible in a canceled state."),
+    ROLE_NOT_CREATOR(HttpStatus.UNAUTHORIZED, "ROLE_NOT_CREATOR", "Only creators can create courses."),
+    COURSE_NOT_FOUND(HttpStatus.NOT_FOUND, "COURSE_NOT_FOUND", "Course is not founded");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+
+    ErrorCode(HttpStatus status, String code, String message) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+
 }
