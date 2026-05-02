ceca75b821077062f2966fd47fc737dd4f7fa438
sommaanny <a000626@naver.com>
2026-05-02 20:07:09 +0900

    dev course dto

--- src/main/java/com/liveclass/be_a/domain/course/dto/CourseResponseDto.java
+++ src/main/java/com/liveclass/be_a/domain/course/dto/CourseResponseDto.java
@@ -1,2 +1,34 @@
-package com.liveclass.be_a.domain.course.dto;public class CourseRespnseDto {
+package com.liveclass.be_a.domain.course.dto;
+
+import com.liveclass.be_a.domain.course.entity.Course;
+import com.liveclass.be_a.domain.course.entity.CourseStatus;
+
+import java.time.LocalDateTime;
+
+public record CourseResponseDto(
+        Long id,
+        String title,
+        String description,
+        int price,
+        int capacity,
+        String status,
+        LocalDateTime startDate,
+        LocalDateTime endDate,
+        boolean isEnrollable //강의 신청이 가능한 상태인지
+) {
+    //entity -> DTO 변환 메서드
+    public static CourseResponseDto from(Course course) {
+        return new CourseResponseDto(
+                course.getId(),
+                course.getTitle(),
+                course.getDescription(),
+                course.getPrice(),
+                course.getCapacity(),
+                course.getStatus().name(),
+                course.getStartDate(),
+                course.getEndDate(),
+                // 현재 모집 중(OPEN)이면서 종료일이 지나지 않았는지 계산
+                course.getStatus() == CourseStatus.OPEN && LocalDateTime.now().isBefore(course.getEndDate())
+        );
+    }
 }
