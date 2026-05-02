590e671118240d4c0dba5a844f4c40015f229db0
sommaanny <a000626@naver.com>
2026-05-02 11:04:02 +0900

    init

---
+++ src/main/java/com/liveclass/be_a/BeAApplication.java
@@ -0,0 +1,13 @@
+package com.liveclass.be_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liveclass/be_a/BeAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liveclass.be_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
