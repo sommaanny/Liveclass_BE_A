0d3e1eb80d522c2e5869b72250abc8330ab5384f
sommaanny <a000626@naver.com>
2026-05-03 16:25:24 +0900

    add getter

--- src/main/java/com/liveclass/be_a/global/exception/BusinessException.java
+++ src/main/java/com/liveclass/be_a/global/exception/BusinessException.java
@@ -1,8 +1,11 @@
 package com.liveclass.be_a.global.exception;
 
+import lombok.Getter;
+
 /**
  * 커스텀 런타임 예외
  */
+@Getter
 public class BusinessException extends RuntimeException{
     private final ErrorCode errorCode;
 
