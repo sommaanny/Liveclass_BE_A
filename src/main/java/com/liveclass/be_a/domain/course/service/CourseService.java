16cb611b9e6618d317e4987318394cd9441f1fa6
sommaanny <a000626@naver.com>
2026-05-03 18:34:51 +0900

    change return type void -> Long

--- src/main/java/com/liveclass/be_a/domain/course/service/CourseService.java
+++ src/main/java/com/liveclass/be_a/domain/course/service/CourseService.java
@@ -26,21 +26,22 @@ public class CourseService {
 
     //강의 등록, 크리에이터만 가능해야함
     @Transactional
-    public void createCourse(CourseRequestDto courseRequestDto) {
+    public Long createCourse(CourseRequestDto courseRequestDto) {
         //간략한 권한 체크(크리에이터인지 확인)
         if (!courseRequestDto.role().equals("CREATOR")) {
             throw new BusinessException(ErrorCode.ROLE_NOT_CREATOR);
         }
         Course course = courseRequestDto.toEntity();
         courseRepository.save(course);
+
+        return course.getId();
     }
 
     //강의 오픈
     @Transactional
-    public void open(Long courseId, Long creatorId) {
+    public Long open(Long courseId, Long creatorId) {
         //id에 해당하는 강의 조회
-        Course course = courseRepository.findById(courseId)
-                .orElseThrow(() -> new BusinessException(ErrorCode.COURSE_NOT_FOUND));
+        Course course = getCourse(courseId);
 
         //간략한 검증, 강좌를 개설한 강사만 상태를 변결할 수 있음
         if(!creatorId.equals(course.getCreatorId())) {
@@ -49,14 +50,15 @@ public class CourseService {
 
         //강좌 오픈
         course.openCourse();
+
+        return course.getId();
     }
 
     //강의 마감
     @Transactional
-    public void close(Long courseId, Long creatorId) {
+    public Long close(Long courseId, Long creatorId) {
         //id에 해당하는 강의 조회
-        Course course = courseRepository.findById(courseId)
-                .orElseThrow(() -> new BusinessException(ErrorCode.COURSE_NOT_FOUND));
+        Course course = getCourse(courseId);
 
         //간략한 검증
         if(!creatorId.equals(course.getCreatorId())) {
@@ -65,6 +67,8 @@ public class CourseService {
 
         //강좌 마감
         course.closeCourse();
+
+        return course.getId();
     }
 
     //강의 목록 조회(상태 필터 가능)
@@ -78,8 +82,7 @@ public class CourseService {
     //강의 상세 조회
     public CourseResponseDto getCourseDetail(Long courseId) {
         //강의 조회
-        Course course = courseRepository.findById(courseId)
-                .orElseThrow(() -> new BusinessException(ErrorCode.COURSE_NOT_FOUND));
+        Course course = getCourse(courseId);
 
         //해당 강좌의 수강신청 인원 카운트
         int cnt = enrollmentRepository.countEnrollments(courseId);
@@ -88,4 +91,9 @@ public class CourseService {
         return CourseResponseDto.from(course, cnt);
     }
 
+    private Course getCourse(Long courseId) {
+        Course course = courseRepository.findById(courseId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.COURSE_NOT_FOUND));
+        return course;
+    }
 }
