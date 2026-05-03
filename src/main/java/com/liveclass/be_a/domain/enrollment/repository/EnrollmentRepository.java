0d3a058e658ef1de1652c231c3534e587aa8181f
sommaanny <a000626@naver.com>
2026-05-04 08:54:58 +0900

    add findById

--- src/main/java/com/liveclass/be_a/domain/enrollment/repository/EnrollmentRepository.java
+++ src/main/java/com/liveclass/be_a/domain/enrollment/repository/EnrollmentRepository.java
@@ -10,6 +10,7 @@ import java.util.Optional;
  */
 public interface EnrollmentRepository {
     public void save(Enrollment enrollment); //수강신청 저장
+    public Optional<Enrollment> findById(Long id); //enrollment id로 조회
     public List<Enrollment> findByMemberId(Long memberId); //수강신청 찾기 (내 수강 신청 목록 조회를 위해)
     public int countEnrollments(Long courseId); //특정 강좌의 수강신청 카운트(현재 신청 인원 조회를 위해)
 }
