42ce840748a04f370e049f58571b631240779ede
sommaanny <a000626@naver.com>
2026-05-04 09:24:45 +0900

    dev EnrollmentService

---
+++ src/main/java/com/liveclass/be_a/domain/enrollment/service/EnrollmentService.java
@@ -0,0 +1,103 @@
+package com.liveclass.be_a.domain.enrollment.service;
+
+import com.liveclass.be_a.domain.course.entity.Course;
+import com.liveclass.be_a.domain.course.repository.CourseRepository;
+import com.liveclass.be_a.domain.enrollment.dto.EnrollmentResponseDto;
+import com.liveclass.be_a.domain.enrollment.entity.Enrollment;
+import com.liveclass.be_a.domain.enrollment.repository.EnrollmentRepository;
+import com.liveclass.be_a.domain.member.entity.Member;
+import com.liveclass.be_a.domain.member.repository.MemberRepository;
+import com.liveclass.be_a.global.exception.BusinessException;
+import com.liveclass.be_a.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * 수강신청 관리 서비스
+ */
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class EnrollmentService {
+    private final EnrollmentRepository enrollmentRepository;
+    private final MemberRepository memberRepository;
+    private final CourseRepository courseRepository;
+
+    //수강 신청, 강의별 최대 정원을 초과한 신청은 거부(동시성 고려)
+    @Transactional
+    public Long enrollment(Long courseId, Long memberId) {
+        //신청 회원 조회
+        Member member = getMember(memberId);
+
+        //강의 조회 with 비관적 락 -> 강의 조회를 순차적으로 진행
+        Course course = courseRepository.findByIdWithLock(courseId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.COURSE_NOT_FOUND));
+
+        //강의의 정원이 초과되지 않았는지 확인
+        int currentCount = enrollmentRepository.countEnrollments(courseId); //현재 수강 신청 인원
+        if (currentCount >= course.getCapacity()) {
+            throw new BusinessException(ErrorCode.COURSE_CAPACITY_FULL); //만석이면 예외 throw
+        }
+
+        //수강 신청 엔티티 생성 및 저장
+        Enrollment enrollment = Enrollment.createEnrollment(course, member);
+        enrollmentRepository.save(enrollment);
+
+        return enrollment.getId();
+    }
+
+    //결제 완료
+    @Transactional
+    public Long payment(Long enrollmentId) {
+        //수강신청 조회
+        Enrollment enrollment = getEnrollment(enrollmentId);
+
+        //결제 완료, 수강 확정 -> 단순 상태 변경
+        enrollment.confirm();
+
+        return enrollment.getId();
+    }
+    //수강 취소
+
+    @Transactional
+    public Long cancel(Long enrollmentId) {
+        //수강신청 조회
+        Enrollment enrollment = getEnrollment(enrollmentId);
+
+        enrollment.cancel(); //수강 취소
+
+        return enrollment.getId();
+    }
+
+    //내 수강 신청 목록 조회
+    public List<EnrollmentResponseDto> findMemberEnrollments(Long memberId) {
+        //회원이 존재하는지 체크
+        if (!memberRepository.existsById(memberId)) {
+            throw new BusinessException(ErrorCode.MEMBER_NOT_FOUND);
+        }
+
+        //memberId로 수강신청 조회
+        List<Enrollment> enrollments = enrollmentRepository.findByMemberId(memberId);
+
+        //DTO로 변환해서 반환
+        return enrollments.stream()
+                .map(EnrollmentResponseDto::from)
+                .toList();
+    }
+
+    private Enrollment getEnrollment(Long enrollmentId) {
+        Enrollment enrollment = enrollmentRepository.findById(enrollmentId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.ENROLLMENT_NOT_FOUND));
+        return enrollment;
+    }
+
+    private Member getMember(Long memberId) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_FOUND));
+        return member;
+    }
+}
